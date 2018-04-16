67d1f7ad53a6722b992f7209e61600be1c7f9787
arul <arul@arul>
2018-04-16 18:55:20 +0530

    Add Filter Service, Controller, Model and Route path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,46 +8,49 @@ import { FormsModule } from '@angular/forms';
 
 import { Approute } from './app.routes';
 
+import { AdminComponent } from './admin/admin.component';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HeaderSearchComponent } from './header-search/header-search.component';
-import { HeaderNavigationComponent } from './header-navigation/header-navigation.component';
 import { BredcrumbComponent } from './bredcrumb/bredcrumb.component';
-import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailviewComponent } from './detailview/detailview.component';
+import { FooterComponent } from './footer/footer.component';
 import { FilterComponent } from './filter/filter.component';
+import { FilterFormComponent } from './filter-form/filter-form.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderNavigationComponent } from './header-navigation/header-navigation.component';
+import { HeaderSearchComponent } from './header-search/header-search.component';
 import { HomeSectionComponent } from './home-section/home-section.component';
-import { ViewComponent } from './view/view.component';
+import { HostelComponent } from './hostel/hostel.component';
 import { ListComponent } from './list/list.component';
 import { ListContentComponent } from './list-content/list-content.component';
 import { ListReviewsComponent } from './list-reviews/list-reviews.component';
-import { HostelComponent } from './hostel/hostel.component';
-import { AdminComponent } from './admin/admin.component';
+import { ViewComponent } from './view/view.component';
+
 // import {HttpdataService} from './service/httpdata.service';
 
 
 import { DefaultsService } from './services/defaults.service';
-import {NgxGalleryModule} from 'ngx-gallery'
-import { DetailviewComponent } from './detailview/detailview.component';
+import { NgxGalleryModule } from 'ngx-gallery'
 
 @NgModule({
   declarations: [
+    AdminComponent,
     AppComponent,
+    BredcrumbComponent,
+    DashboardComponent,
+    DetailviewComponent,
+    FooterComponent,
+    FilterComponent,
+    FilterFormComponent,
     HeaderComponent,
     HeaderSearchComponent,
     HeaderNavigationComponent,
-    BredcrumbComponent,
-    FooterComponent,
-    DashboardComponent,
-    FilterComponent,
-HomeSectionComponent,
-    ViewComponent,
+    HomeSectionComponent,
+    HostelComponent,
     ListComponent,
     ListContentComponent,
     ListReviewsComponent,
-    DetailviewComponent,
-    HostelComponent,
-    AdminComponent
+    ViewComponent
   ],
   imports: [
     BrowserModule,
