dfd18ece5e130dfeee5b8de2e3513d92f2059777
arul <arul@arul>
2018-04-19 13:03:52 +0530

    Add Filter Service, Controller, Model and Route path

--- src/app/services/defaults.service.ts
+++ src/app/services/defaults.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+import { Globals } from '../utils/globals';
 
 @Injectable()
 export class DefaultsService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private _globals: Globals) { }
+  readonly baseUrl = this._globals.baseAppUrl;
 
   private BASE_URL="../assets/json/";
 
@@ -15,5 +18,20 @@ export class DefaultsService {
   getHostels():any{
     let hostelUrl=""
     return this.http.get(this.BASE_URL+"hostels.json").map((response:Response)=>response);
+  }
+
+  getAllHostelData(): Observable<any[]> {
+    return this.http.get(this.baseUrl + "/hostel")
+      .map(this.extractData)
+      .catch(this.handleErrorObservable);
+  }
+
+  private extractData(res: Response) {
+    let body = res;
+    return body || {};
+  }
+
+  private handleErrorObservable(error: Response | any) {
+    return Observable.throw(error.message || error);
   }
 }
