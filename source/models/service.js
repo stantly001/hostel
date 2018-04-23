df65b2bb9dab017eace2eb8731ec44019bd22d1b
stantly001 <stantly001@gmail.com>
2018-04-23 12:44:22 +0530

    April 16th 2018 : ------------------ UI Changes and bug fixes done

--- source/models/service.js
+++ source/models/service.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-// Define collection and schema for Hostel Items
+// Define collection and schema for Service Items
 var ServiceSchema = new Schema({
     service_name: {
         type: String
