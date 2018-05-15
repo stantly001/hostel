3ec0146f25f80a60e3e672c199a231683a8695c9
stantly001 <stantly001@gmail.com>
2018-05-15 18:40:48 +0530

    Merge origin/hms-P1-Arul-a1 into hms-P1-stantly-s1
    Conflicts:
    	src/app/header-search/header-search.component.ts
    	src/app/hostel/hostel.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Output } from '@angular/core';
 
 import { HttpdataService } from '../service/httpdata.service';
 import { Router, ActivatedRoute, Route } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +12,19 @@ import { Router, ActivatedRoute, Route } from '@angular/router';
   providers: [HttpdataService]
 })
 export class LoginComponent implements OnInit {
-  loginUsers=[];
-  login={};
+  loginUsers = [];
+  login = {};
   loginForms: any[];
   errorMessage: string;
   constructor(private _httpDataService: HttpdataService, private activatedRoute: ActivatedRoute, private route: Router) { }
-
+  // private userDetails = new BehaviorSubject<Object>({});
+  // public setUserDetails(data: Object) {
+  //   this.userDetails.next(data);
+  // } 
+  // public getUserDetails():Observable<any> {
+  //   return this.userDetails.asObservable();;
+  // }
+  
   ngOnInit() {
   }
 
@@ -23,21 +32,22 @@ export class LoginComponent implements OnInit {
    * 
    * @param login 
    */
-  
+
   authSubmit(login) {
     console.log(login);
     if (login) {
       this._httpDataService.authUserLogin(login).subscribe(
         data => {
-          if(data.data.isValid==true){
-          sessionStorage.setItem("user", JSON.stringify(data));
-          let userId = data.data._id;
-          this.loginUsers = data;//.push(data});
-          this.route.navigate(["admin/hostel/addHostel"]);
+          if (data.data.isValid == true) {
+            sessionStorage.setItem("user", JSON.stringify(data));
+            let userId = data.data._id;
+            // this.setUserDetails(data);
+            this.loginUsers = data;//.push(data});
+            this.route.navigate(["admin/hostel/addHostel"]);
           }
           console.log(data)
         },
-        
+
         error => this.errorMessage = <any>error)
     }
   }
